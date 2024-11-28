09db53bd282e927750acfdd8fd347579e1a207a3
manvindarsingh1988 <78023631+manvindarsingh1988@users.noreply.github.com>
2024-11-28 14:53:19 +0530

    Fund Transfer Changes added

--- Script/Script2.sql
+++ Script/Script2.sql
@@ -24,12 +24,14 @@ Go
 
 
  Alter PROC [dbo].[usp_updateUserActivationState] 
-    @RetailUserId varchar(14)
+    @RetailUserId varchar(14),
+	@allUser tinyint
 AS 
 	DECLARE @Active tinyint 
 	Declare @OperationMessage nvarchar(500)
 	DECLARE @RetailUserOrderNo INT
-	SELECT @Active = Active, @RetailUserOrderNo = OrderNo from RetailUser WITH(NOLOCK) WHERE Id=@RetailUserId;
+	Declare @UserType int
+	SELECT @Active = Active, @RetailUserOrderNo = OrderNo, @UserType = UserType from RetailUser WITH(NOLOCK) WHERE Id=@RetailUserId;
 	if(@Active = 1)
 	begin 
 	   Set @Active = 0
@@ -39,6 +41,29 @@ AS
 	   Set @Active = 1
     end
 	UPDATE RetailUser SET [Active]=@Active, KYCRequired = 0 WHERE Id=@RetailUserId;
+	if(@allUser = 1 and (@UserType = 6 or @UserType = 7))
+	begin
+	 if(@Active = 0)
+	 begin 
+	   UPDATE RetailUser SET [Active]=@Active WHERE MasterId= @RetailUserId
+	   if (@UserType = 7)
+	   begin
+	    UPDATE RetailUser SET [Active]=@Active WHERE MasterId in (Select Id from RetailUser where MasterId= @RetailUserId)
+       end
+	 end
+	 else
+	 begin
+	   UPDATE RetailUser SET [Active]= case when (KYCRequired = 1 or ActivatedTill is not null) then 0 else  @Active end,
+	   ActivatedTill = null
+	   WHERE MasterId= @RetailUserId
+	   if (@UserType = 7)
+	   begin
+	    UPDATE RetailUser SET [Active]= case when (KYCRequired = 1 or ActivatedTill is not null) then 0 else  @Active end,
+	   ActivatedTill = null 
+		WHERE MasterId in (Select Id from RetailUser where MasterId= @RetailUserId)
+       end
+	 end
+	end
 	if(@Active = 1)
 	begin 
 	   SELECT @OperationMessage =  'Success: user ' + CONVERT(VARCHAR(20),@RetailUserOrderNo) + ' activated.'
