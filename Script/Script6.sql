4373a912ffe598955c7c69312f3391d7f5541740
manvindarsingh1988 <78023631+manvindarsingh1988@users.noreply.github.com>
2024-12-12 13:03:31 +0530

    date check updated

--- Script/Script6.sql
+++ Script/Script6.sql
@@ -1,4 +1,4 @@
-create procedure usp_RetailUserListWithBalanceByUserAndDate
+Alter procedure usp_RetailUserListWithBalanceByUserAndDate
 @date datetime,
 @Id varchar(10)
 As
@@ -9,7 +9,7 @@ Begin
 	Mobile AS MobileNumber,      
 	MasterId as Parent, ISNULL([dbo].[RetailClientOrderNoUserNameMobile](MasterId),'') AS ParentName,
 	City, EMail, [Address], Active,
-	(Select Top 1 ClosingBalance from RTran where RetailUserId = RU.Id and cast(CreateDate as date) = cast(@date as date) order by CreateDate desc) Balance
+	(Select Top 1 ClosingBalance from RTran where RetailUserId = RU.Id and cast(CreateDate as date) <= cast(@date as date) order by CreateDate desc) Balance
 	FROM RetailUser RU WITH(NOLOCK)  
 	where 1 = case when ((@Id is not null and OrderNo = @Id) or @Id is null) then 1 else 0 end 
 	ORDER BY OrderNo ASC 
