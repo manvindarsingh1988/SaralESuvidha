660734f4f8f132b413226c89361d604d1d675612
prabhatmohan <prabhatm@practicsoft.com>
2024-12-13 17:07:15 +0530

    tran monitor

---
+++ SaralESuvidha/ViewModel/TransactionLog.cs
@@ -0,0 +1,11 @@
+namespace SaralESuvidha.ViewModel;
+
+public class TransactionLog
+{
+    public string RechargeMobileNumber { get; set; }
+    public string Amount { get; set; }
+    public string RechargeStatus { get; set; }
+    public string UPPCL_Discom { get; set; }
+    public string CreateDate { get; set; }
+    
+}
