63038bbaa64c4802e8042eea3f84f04cbfff21c8
manvindarsingh1988 <78023631+manvindarsingh1988@users.noreply.github.com>
2025-03-31 16:48:16 +0530

    secondary admin changes added

---
+++ SaralESuvidha/Controllers/SecondaryAdminController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SaralESuvidha.Filters;
+using System;
+
+namespace SaralESuvidha.Controllers
+{
+    [SecondaryAdminFilter]
+    public class SecondaryAdminController : Controller
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public SecondaryAdminController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View("~/Views/SysAdmin/Index.cshtml");
+        }
+
+        public IActionResult ListAllUserWithBalance()
+        {
+            return View("~/Views/SysAdmin/ListAllUserWithBalance.cshtml");
+        }
+
+        public IActionResult ListMasterDistributor()
+        {
+            ViewData["UserType"] = HttpContext.Session.GetInt32("UserType") != null ? HttpContext.Session.GetInt32("UserType").ToString() : string.Empty;
+            return View("~/Views/SysAdmin/ListMasterDistributor.cshtml");
+        }
+
+        public IActionResult RefundList()
+        {
+            return View("~/Views/SysAdmin/RefundList.cshtml");
+        }
+
+        //reports 
+        public IActionResult DailySalesReport()
+        {
+            return View("~/Views/SysAdmin/DailySalesReport.cshtml");
+        }
+
+        public IActionResult DailyStatement()
+        {
+            return View("~/Views/SysAdmin/DailyStatement.cshtml");
+        }
+
+        public IActionResult ClientStatement()
+        {
+            return View("~/Views/SysAdmin/ClientStatement.cshtml");
+        }
+
+        public IActionResult ClientFundReport()
+        {
+            return View("~/Views/SysAdmin/ClientFundReport.cshtml");
+        }
+
+        public IActionResult AccountTopupReport()
+        {
+            return View("~/Views/SysAdmin/AccountTopupReport.cshtml");
+        }
+
+        public IActionResult SearchRecharge()
+        {
+            return View("~/Views/SysAdmin/SearchRecharge.cshtml");
+        }
+
+        public IActionResult UserReport()
+        {
+            return View("~/Views/SysAdmin/UserReport.cshtml");
+        }
+
+        public IActionResult UserCommissionReport()
+        {
+            return View("~/Views/SysAdmin/UserCommissionReport.cshtml");
+        }
+
+        public IActionResult UPPCLDailyReport()
+        {
+            return View("~/Views/SysAdmin/UPPCLDailyReport.cshtml");
+        }
+
+        public IActionResult SystemSetting()
+        {
+            return View("~/Views/SysAdmin/SystemSetting.cshtml");
+        }
+
+        public IActionResult ChangePassword()
+        {
+            return View("~/Views/SysAdmin/ChangePassword.cshtml");
+        }
+
+        public IActionResult Logout()
+        {
+            try
+            {
+                HttpContext.Session.Clear();
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                return Content("Exception: " + ex.Message);
+            }
+        }
+
+    }
+}
