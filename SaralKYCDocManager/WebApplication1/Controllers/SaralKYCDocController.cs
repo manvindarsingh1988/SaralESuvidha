96057134f0a32752ea508b79e5097ca3af921fc1
manvindarsingh1988 <78023631+manvindarsingh1988@users.noreply.github.com>
2025-06-03 19:47:47 +0530

    file upload/download for cashflow

--- SaralKYCDocManager/WebApplication1/Controllers/SaralKYCDocController.cs
+++ SaralKYCDocManager/WebApplication1/Controllers/SaralKYCDocController.cs
@@ -67,6 +67,55 @@ namespace WebApplication1.Controllers
             
             return new FileItem { Content = result, FileName = fileName };
         }
+
+        [HttpPost]
+        [Route("UploadCashFlowFile")]
+        public IActionResult UploadCashFlowFile([FromBody] FileItem file)
+        {
+            if (file == null)
+                return BadRequest("No files provided for zipping.");
+            string folderPath = Path.Combine("CashFlowFiles/" + file.FileName + "/");
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            var fileName = file.FileName;
+            fileName = "CashFlowFiles/" + file.FileName + ".zip";
+            if (System.IO.File.Exists(fileName))
+            {
+                System.IO.File.Delete(fileName);
+            }
+            System.IO.File.WriteAllBytes(fileName, file.Content);
+            ZipFile.ExtractToDirectory(fileName, folderPath, Encoding.UTF8, true);
+            if (System.IO.File.Exists(fileName))
+            {
+                System.IO.File.Delete(fileName);
+            }
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("DownloadCashFlowFile")]
+        public FileItem DownloadCashFlowFile(string fileName)
+        {
+            byte[] result = Array.Empty<byte>();
+            string folderPath = Path.Combine("CashFlowFiles/");
+            folderPath = folderPath + fileName;
+
+            if (System.IO.Directory.Exists(folderPath))
+            {
+                var fn = "CashFlowFiles/" + fileName + ".zip";
+                if (System.IO.File.Exists(fn))
+                {
+                    System.IO.File.Delete(fn);
+                }
+                ZipFile.CreateFromDirectory(folderPath, fn);
+                result = System.IO.File.ReadAllBytes(fn);
+                System.IO.File.Delete(fn);
+            }
+
+            return new FileItem { Content = result, FileName = fileName };
+        }
     }
 
     public class FileItem
